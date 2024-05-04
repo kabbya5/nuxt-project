54e0cdedcdffc93f1283567c8d7df0ed2b000fe3
Quaomiuzzaman Kabbya <kabbya44@gmail.com>
2024-05-04 23:04:58 +0600

    handel client side error

--- components/Car/SideBar.vue
+++ components/Car/SideBar.vue
@@ -4,11 +4,12 @@
     <div class="shadow border w-64 mr-10 z-30 h-[190px]">
         <div class="p-5 flex justify-between relative cursor-pointer border-b">
             <h3>Location</h3>
-            <h3 class="text-blue capitalize"> Toronto</h3>
-            <!-- <div class="aboslute border shadow left-56 p-5 top-1 -m-1">
-                <input type="text" class="border p-1 rounded">
-                <button class="bg-blue-400 w-full mt-2 rounded text-white p-1"> Apply </button>
-            </div> -->
+            <h3 @click="updateModal('location')" class="text-blue capitalize"> {{ route.params.city }} </h3>
+
+            <div v-if="modal.location" class="aboslute border shadow left-56 p-5 top-1 -m-1">
+                <input type="text" class="border p-1 rounded" v-model="city">
+                <button @click="changeLocation" class="bg-blue-400 w-full mt-2 rounded text-white p-1"> Apply </button>
+            </div>
         </div>
         <div class="p-5 flex justify-between relative cursor-pointer border-b">
             <h3>Make</h3>
@@ -18,3 +19,34 @@
     <!-- CAR SIDE BAR  -->
 </div>
 </template>
+
+
+<script setup> 
+    const modal = ref({
+        make:false,
+        location:false,
+        price:false,
+    });
+
+    const city = ref("");
+    const route = useRoute();
+    const updateModal = (key) =>{
+        modal.value[key] = !modal.value[key];
+    }
+
+    const changeLocation = () => {
+        if(!city.value) return;
+        if(!isNaN(parseInt(city.value))){
+            throw createError({
+                statusCode:404,
+                message:"Invalid City format",
+            });
+        }
+        updateModal('location');
+        navigateTo(`/city/${city.value}/car/${route.params.make}`);
+        city.value = ' ';
+         
+    }
+    
+     
+</script>
--- pages/city/[city]/car.vue
+++ pages/city/[city]/car.vue
@@ -4,8 +4,19 @@
         <div class="w-full flex justify-center mt-0">
             <div class="mx-auto w-[1200px] space-y-4 px-4">
                 <div class="mt-32 flex">
-                    <CarSideBar/>
-                    <NuxtPage/>
+                    <NuxtErrorBoundary> 
+                        <CarSideBar/>
+                        <NuxtPage/>
+
+                        <template #error="{error}">
+                            <div class="text-center mx-auto flex flex-col">
+                                <h1 class="text-5xl text-red-500"> Sorry, Something went wrong</h1>
+                                <code class="my-6"> {{ error }}</code>
+
+                                <button class="text-white bg-blue-400 px-10 py-3 rounded mt-4" @click="error.value = null"> Go Back </button>
+                            </div>
+                        </template>
+                    </NuxtErrorBoundary>
                 </div>
             </div>
         </div>
